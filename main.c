60cf576f00ecf4745cecda399281a3b136fa3cf2
Angelmare <Angelmare@users.noreply.github.com>
2014-10-07 22:59:56 +0200

    Ajout fichier ToDo List

--- main.c
+++ main.c
@@ -6,7 +6,11 @@ void main(){
 	}//endwhile
 }//endmain
 
+<<<<<<< HEAD
 int calculeMePlease(int me){
 	printf("No, i really don't want, you're %hd too much... sorry >_< \n",me);
 	return me;
 }//endfunction
+=======
+}//endmain
+>>>>>>> parent of 04ef76e... Preparation main.c avec comments
